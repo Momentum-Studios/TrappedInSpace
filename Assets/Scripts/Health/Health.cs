0e2884d3b6fc1ad30ce95b16f02cfa206b028474
drodriguez97 <danrod.biz@gmail.com>
2022-11-27 12:32:51 -0800

    Adding Player, Camera Prefab & Layers

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,84 +4,86 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
-    
-    [Header("Health")]
-    [SerializeField] private float StartingHealth;
 
+    [Header("Health")]
+    [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
-    private bool dead;
     private Animator anim;
-    public Image HealthSlider;
-    
+    private bool dead;
+
     [Header("iFrames")]
     [SerializeField] private float iFramesDuration;
-    [SerializeField] private float numberofFlashes;
-
+    [SerializeField] private int numberOfFlashes;
     private SpriteRenderer spriteRend;
 
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
+
+    [Header("Death Sound")]
+    [SerializeField] private AudioClip deathSound;
+    [SerializeField] private AudioClip hurtSound;
 
     private void Awake()
     {
-        currentHealth = StartingHealth;
+        currentHealth = startingHealth;
         anim = GetComponent<Animator>();
         spriteRend = GetComponent<SpriteRenderer>();
     }
-
-
-    public void TakeDamage(float _damage) 
-    {
-        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, StartingHealth);
-        UpdateHP();
-        if (currentHealth > 0)
-        {
-
-            anim.SetTrigger("hurt");
-            StartCoroutine(Invulnerability());
-
-        }
-        else
-        {
-            if (!dead) 
-            {
-                anim.SetTrigger("die");
-                GetComponent<PlayerMovement>().enabled= false;
-                
-                dead = true;
-
-            }
-           
-        }
-    }
-
     private void Update()
     {
         //Test if the health bar works by pressing E, you should see health disapear
-        if( Input.GetKeyDown(KeyCode.E)) 
-            TakeDamage(1); 
+        if (Input.GetKeyDown(KeyCode.E))
+            TakeDamage(1);
     }
 
-    public void AddHealth(float _value)
-    { 
-        currentHealth = Mathf.Clamp(currentHealth + _value, 0, StartingHealth);
-        UpdateHP();
-    }
-
-    private IEnumerator Invulnerability()
+    public void TakeDamage(float _damage)
     {
-        Physics2D.IgnoreLayerCollision(8, 9, true); //need to know the layers of enemies and players
+        if (invulnerable) return;
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
-        for (int i = 0; i < numberofFlashes; i++)
+        if (currentHealth > 0)
         {
-            spriteRend.color = new Color(1, 0, 0, 0.5f); //change to red when invulnerable
-            yield return new WaitForSeconds(iFramesDuration / (numberofFlashes) * 2);
-            spriteRend.color = Color.white;
-            yield return new WaitForSeconds(iFramesDuration / (numberofFlashes) * 2);
-
+            anim.SetTrigger("hurt");
+            StartCoroutine(Invunerability());
+            //SoundManager.instance.PlaySound(hurtSound);
         }
-        Physics2D.IgnoreLayerCollision(8, 9, false);
-    }
+        else
+        {
+            if (!dead)
+            {
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
 
-    public void UpdateHP(){
-        HealthSlider.fillAmount = ((100/StartingHealth) * currentHealth) / 100;
+                anim.SetBool("grounded", true);
+                anim.SetTrigger("die");
+
+                dead = true;
+                //SoundManager.instance.PlaySound(deathSound);
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+    private IEnumerator Invunerability()
+    {
+        invulnerable = true;
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable = false;
+    }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
