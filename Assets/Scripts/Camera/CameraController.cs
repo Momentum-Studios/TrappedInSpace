e4cc2f4c633249473ae3ea1028e96020ac72b21d
Gregorius Avip <71865513+gavip1@users.noreply.github.com>
2022-11-30 00:52:41 -0800

    Camera background is now black
    Change CameraController script so that the background is colored black

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,8 +8,14 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float aheadDistance;
     [SerializeField] private float cameraSpeed;
 
+    public Camera cam;
     private float lookAhead;
 
+    void Start(){
+        cam = GetComponent<Camera>();
+        cam.clearFlags = CameraClearFlags.SolidColor;
+        cam.backgroundColor = Color.Lerp(Color.black, Color.black, 0);
+    }
     private void Update()
     {
         transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
