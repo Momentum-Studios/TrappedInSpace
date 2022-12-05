01c4992255962139ea5ae6117b71b150d1a607a1
Daniel Nam <89194282+DanisoNam@users.noreply.github.com>
2022-12-05 00:35:14 -0800

    Added Comments to MainMenu.cs and PauseMenu.cs

--- Assets/Scripts/Game Menus/PauseMenu.cs
+++ Assets/Scripts/Game Menus/PauseMenu.cs
@@ -1,3 +1,16 @@
+/**
+ * file: PauseMenu.cs
+ * studio: Momentum Studios
+ * authors: Daniel Nam
+ * class: CS 4700 - Game Development
+ * 
+ * assignment: Program 4
+ * date last modified: 11/30/2022
+ * 
+ * purpose: Allows user to pause the game with the escape key.
+Game is frozen while it is paused. You can resume the game, go to the main menu, or quit the game from 
+the pause screen. 
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
