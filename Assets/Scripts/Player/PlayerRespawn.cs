23f41f4644eea1dedd3852d1afb1a1db78b5b87b
Some-Regular-Person <31082683+Some-Regular-Person@users.noreply.github.com>
2022-12-04 23:44:31 -0800

    Add level reseting

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -1,3 +1,14 @@
+/**
+ * file: PlayerRespawn.cs
+ * studio: Momentum Studios
+ * authors: Daniel Rodriguez, Justin Kim
+ * class: CS 4700 - Game Development
+ * 
+ * assignment: Program 4
+ * date last modified: 11/17/2022
+ * 
+ * purpose: This script acts as the script that handles respawning of players
+ */
 using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
@@ -5,32 +16,34 @@ public class PlayerRespawn : MonoBehaviour
 
     [SerializeField] private AudioSource checkpointSound;
 
-    private Transform currentCheckpoint;
     private PlayerHealth playerHealth;
+    private GameManager gameManager;
 
     private Animator anim;
 
+    // setup script by getting components for this script
     private void Awake()
     {
-        anim= GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         playerHealth = GetComponent<PlayerHealth>();
+        gameManager = GameManager.getInstance();
     }
 
+    // handle respawning player
     public void Respawn()
     {
-        playerHealth.Respawn(); //Restore player health and reset animation
-        transform.position = currentCheckpoint.position; //Move player to checkpoint location
-
+        gameManager.reloadLevel();
     }
+
+    // handle entering checkpoints
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "CheckPoint")
+        if (collision.gameObject.name.ToLower().StartsWith("checkpoint"))
         {
-            anim.Play("IdleFloppy");
             checkpointSound.Play();
-            currentCheckpoint = collision.transform;
+            gameManager.setCheckPoint(new Vector3(collision.transform.position.x, collision.transform.position.y, collision.transform.position.z));
             collision.GetComponent<Collider2D>().enabled = false;
-          
+            print("test");
         }
     }
 }
